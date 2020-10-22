6e92cf5aa30c45114a43f4f911c0cbbedb2e5ab9
John El Basha <johnelbasha@gmail.com>
2020-10-22 17:27:13 +0100

    added services controller, model, seeds and now there is an index page with all the services

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Service.destroy_all
+
+puts 'Creating services...'
+Service.create!(name: 'annual boiler service', photo_url: 'https://www.swaleheating.com/media/wysiwyg/Boiler-service-from.jpg')
+Service.create!(name: 'Emergency call out', photo_url: 'https://twocountiescleaning.co.uk/wp-content/uploads/2017/02/help-emergency.png')
+Service.create!(name: 'annual boiler service', photo_url: 'https://www.stheatingservices.co.uk/wp-content/uploads/2017/06/shutterstock_524040448.jpg')
+
+
+puts 'Finished!'
