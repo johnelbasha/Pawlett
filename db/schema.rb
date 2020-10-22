71e9a2b123abf1293780b98f6d693b405d862d5e
John El Basha <johnelbasha@gmail.com>
2020-10-22 17:02:31 +0100

    added last_name, house_number and postal code to user model/devise registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_153234) do
+ActiveRecord::Schema.define(version: 2020_10_22_154315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 2020_10_22_153234) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "firstname"
+    t.string "last_name"
+    t.string "house_name_or_number"
+    t.string "postal_code"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
