f57dd4a6d49c97d8497eb1923112a924035d03f9
John El Basha <johnelbasha@gmail.com>
2020-11-17 20:44:39 +0000

    added new spec

--- spec/features/visitor_can_register_new_account_spec.rb
+++ spec/features/visitor_can_register_new_account_spec.rb
@@ -1,6 +1,27 @@
 require 'rails_helper'
 
-feature '' do
-  scenario '' do
+feature 'User registers for new account' do
+  scenario 'A visitor is on the homepage and can see the registration button' do
+    visit root_path
+
+    expect(page).to have_css 'a', text: 'Register'
+  end
+
+  scenario 'A visit clicks on the Register button and it directs them to the registration page' do
+    visit root_path
+    click_on 'Register'
+
+    expect(page).to have_css 'h2', text: 'Sign up'
+  end
+
+  scenario 'new registration page has all correct fields' do
+    visit root_path
+    click_on 'Register'
+
+    expect(page).to have_css '#user_email'
+    expect(page).to have_css '#user_firstname'
+    expect(page).to have_css '#user_last_name'
+    expect(page).to have_css '#user_last_name'
+    expect(page).to have_css '#user_last_name'
   end
 end
